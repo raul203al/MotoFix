9113bdb64b9eb4102f13b828f96e63faf8f3669a
raul203al <90871851+raul203al@users.noreply.github.com>
2023-03-14 09:38:46 +0100

    Barra lateral sin estilizar

--- app/src/main/java/com/example/motofix/MainActivity.kt
+++ app/src/main/java/com/example/motofix/MainActivity.kt
@@ -6,17 +6,27 @@ import android.content.Intent
 import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.Button
 import android.widget.CheckBox
-import android.widget.Switch
 import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
 import clases.User
+import com.google.android.material.navigation.NavigationView
 import java.io.*
+
 
 class MainActivity : AppCompatActivity() {
 
     var  usuarioLogado:User?=null;
+
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var toggle: ActionBarDrawerToggle
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,6 +40,22 @@ class MainActivity : AppCompatActivity() {
                 this.usuarioLogado=bundle.getParcelable("usuarioLogado");
             }
         }
+
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        val btn_menu : Button = findViewById<Button>(R.id.btn_menu)
+        setSupportActionBar(toolbar)
+
+        setSupportActionBar(toolbar)
+
+        drawerLayout = findViewById(R.id.drawer_layout)
+        toggle = ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        btn_menu.setOnClickListener {
+            drawerLayout.openDrawer(GravityCompat.START)
+        }
+
 
         val alertButton: Button = findViewById<Button>(R.id.alert_btn)
         val userButton: Button = findViewById<Button>(R.id.userBtn)
