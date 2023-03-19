2de6c85a79cfada75203fce07cc115a63b94a588
Raul <raul203al@gmail.com>
2023-03-19 02:07:40 +0100

    Clase Moto Recycler View en progreso

---
+++ app/src/main/java/com/example/motofix/ActivitySwitch.kt
@@ -0,0 +1,10 @@
+package com.example.motofix
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ActivitySwitch : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
